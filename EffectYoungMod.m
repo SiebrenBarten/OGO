1e8549ff92880b3947c96249584077712fe3b88c
Siebren Barten <125663672+SiebrenBarten@users.noreply.github.com>
2023-02-22 19:01:38 +0100

    Update EffectYoungMod.m

--- EffectYoungMod.m
+++ EffectYoungMod.m
@@ -1,25 +1,30 @@
-Filename = 
-width = 
-thickness = 
+%% Import and process sample measurements                  
+sample_width = 
+sample_thickness = 
+sample_length_orig = 
 
-% Load the data file into a variable called data
-Data = load(Filename);
-
-% Extract the three columns from the data variable
-Dist = Data(:,1);
-Force = Data(:,2);
-Time = Data(:,3);
-
-% Calculate the surface area in m^2
+% Calculate the surface area perpendicular to stress direction in m^2
 act_width = width / 1000; % Convert width from mm to m
 act_thickness = thickness / 1000; % Convert thickness from mm to m
 surface = act_width * act_thickness; % Calculate surface area in m^2
 
+% Convert the original distance from mm to m
+orig_dist = sample_length_orig / 1000; % Convert from mm to m
+
+%% Import tensile test measurements
+% File with 3 columns containing the lentgh, force and time
+Measurements = ; 
+
+% Load the measurement file into a variable called Data
+Data = load(Measurements);
+
+% Extract the columns from Data into arrays
+Dist = Data(:,1);
+Force = Data(:,2);
+Time = Data(:,3);
+
 % Calculate the estimated stress in Pa
 stress = Force ./ surface; % Divide force by surface area
-
-% Convert the original distance from mm to m
-orig_dist = 25.2; % Convert from mm to m
 
 % Calculate the engineering strain
 delta_dist = Dist - orig_dist; % Calculate the change in distance
