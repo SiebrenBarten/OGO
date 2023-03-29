a72046c8a2d7c2c26e23e1dd50a1dbff1e8eb96d
Siebren Barten <125663672+SiebrenBarten@users.noreply.github.com>
2023-03-29 17:00:52 +0200

    Perfect graphs and add parameter correlation
    This Matlab file is now a function that is able to plot the effect of the volume fraction and orientation of the secondary fiber with respect to a certain pulling direction (here: the direction of the primary fiber). The function is also able to mark the intersection line between the graphs and give the equation of this line in the form of a 1st-order polynomial in a string.

--- SecondaryFiberMesh.m
+++ SecondaryFiberMesh.m
@@ -1,54 +1,78 @@
-clear all; % Clear the workspace 
+function [] = SecondaryFiberMesh(E_eff)
 
-%% Parameters
+%% Parameters for Meshgrid
 
 % Parameters (Young's modulus)
 nu_m = 0.14;          % [-], Poisson's ratio of the matrix  %functie
-E_fiber1 =  5.35e+08; % [Pa], Young's modulus for fiber (PCL) 
-E_matrix = 1.18e+06;  % [Pa], Young's modulus for matrix 
-E_fiber2 = E_fiber1;     % [Pa], Young's modulus of the second fiber
-
-% Parameters (Young's modulus of the real pleura) 
-E_eff0 = ones(100,100) .* 2.04e7;  % [Pa], Young's modulus for 0 degrees
-E_eff30 = ones(100,100) .* 5.58e6; % [Pa], Young's modulus for 30 degrees
-E_eff60 = ones(100,100) .* 5.3e6;  % [Pa], Young's modulus for 60 degrees
-E_eff90 = ones(100,100) .* 1.28e7; % [Pa], Young's modulus for 90 degrees
+E_fiber =  5.35e+08;  % [Pa], Young's modulus for fiber (PCL) 
+E_matrix = 1.18e+06;  % [Pa], Young's modulus for matrix (silicon
+E_base = 2.89e+06;    % [Pa], Young's modulus of the sample with 9 PCL fibers at 0 degree
 
 % Parameters (Orientations)
-theta = linspace(0, 90, 100);  % [degrees], Range of angles to plot, with respect to the load. 
-alpha = 0;        % [degrees], (Theta-Angle = angle of fiber 1 [PCL]) 
-alpha_fiber2 = 90; % angle of the second fiber with respect to the applied load
+theta = linspace(0, 90, 100);  % [degrees], Matrix with ange of angles between the primary (at 0 degrees)
 
-% Parameters (Volume Fractions) 
-V_fiber1 = 0.7;  % [-], Volume fraction of the first fiber (PCL fiber)
-V_fiber2 = linspace(0, 0.7, 100); % [-], Volume fraction of the second fiber
+% Parameter (Volume Fractions) 
+V_fiber2 = linspace(0, 1, 100); % [-], Matrix with range of volume fraction of the secondary fibers
 
-%% Formula's Effective Young's modulus
+%% Parameters for reference surfaces in Meshgrid
+% Parameters (Young's modulus of the real pleura) 
+E_effective = E_eff * ones(100,100);
 
-%create meshgrid of V_f and theta
+%% Determine the Effective Young's modulus of the primary + secondary fibers
+
+% Create meshgrid of the volume fraction and and orientation of the secondary fiber
 [V_f2_mesh, theta_mesh] = meshgrid(V_fiber2, theta);
 
-% Effective Young's Modulus for each fiber
-E_eff_total = E_matrix .* (1 + nu_m .* ((V_fiber1 + V_f2_mesh) .* (E_fiber1 + E_fiber2) ./ E_matrix - 1) .* cosd(theta_mesh).^2) ./ (1 - (V_fiber1 + V_fiber2) .* nu_m .* cosd(theta_mesh).^2);
+% Calculate the Effective Young's Modulus for the secondary fiber set
+E_eff2 = E_matrix .* (1 + nu_m .* (V_f2_mesh .* E_fiber ./ E_matrix - 1) .* cosd(theta_mesh).^2) ./ (1 - V_f2_mesh .* nu_m .* cosd(theta_mesh).^2);
 
-% plot the surface plot
-surf(V_f2_mesh, theta_mesh, E_eff_total);
-xlabel('V_f');
-ylabel('theta (degrees)');
-zlabel('E_{eff}');
+% Calculate new total Effective Young's Modulus
+E_eff_total = (E_base + E_eff2) ./ 2;
 
-title('Effective Modulus vs. Fiber Volume Fraction and Angle');
-
-hold on; 
-surf(V_f2_mesh, theta_mesh, E_eff0);
-
-hold on; 
-surf(V_f2_mesh, theta_mesh, E_eff30);
+%% Plot the Meshgrid
+% Plot the surface plot
+surf(V_f2_mesh, theta_mesh, E_eff_total);   % Make the Meshgrid and define te axises
+xlabel('V_f');                              % Name the x-axis
+ylabel('theta (degrees)');                  % Name the y-axis
+zlabel('E_{eff}');                          % Name the z-axis
+title('Effective Modulus vs. Fiber Volume Fraction and Angle'); % Add a title to the graph
 
 hold on; 
-surf(V_f2_mesh, theta_mesh, E_eff60);
+surf(V_f2_mesh, theta_mesh, E_effective); 
 
-hold on; 
-surf(V_f2_mesh, theta_mesh, E_eff90);
+%hold on; 
+%surf(V_f2_mesh, theta_mesh, E_eff30);
+
+%hold on; 
+%surf(V_f2_mesh, theta_mesh, E_eff60);
+
+%hold on; 
+%surf(V_f2_mesh, theta_mesh, E_eff90);
 %hold off;
 
+%% Determine the intersection of the graphs 
+
+%idx = find(abs(E_eff_total - E_eff0) == 0);
+%[V_f_intersect, theta_intersect] = ind2sub(size(E_eff_total), idx);
+
+E_diff = E_eff_total - E_effective;
+C = contours(V_f2_mesh, theta_mesh, E_diff, [0 0]);
+
+% Extract the x- and y-locations from the contour matrix C.
+V_f_L = C(1, 2:end);
+theta_L = C(2, 2:end);
+
+% Interpolate on the first surface to find z-locations for the intersection line.
+ELoc = interp2(V_f2_mesh, theta_mesh, E_eff_total, V_f_L, theta_L);
+
+line(V_f_L, theta_L, ELoc, 'Color', 'r', 'LineWidth', 3);
+
+%% Get the equation for the line
+p = polyfit(V_f_L, theta_L, 1); % Fit a first-degree polynomial (linear model) to the data
+
+% Get slope and intercept
+slope = p(2);
+intercept = p(1);
+
+% Print equation of the line
+fprintf('Correlation: theta = %.2f*V_f + %.2f\n', slope, intercept);
