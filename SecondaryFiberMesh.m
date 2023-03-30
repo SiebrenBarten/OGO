16dd8f053da686fb95accc2ed0ce000cf730dd83
Siebren Barten <125663672+SiebrenBarten@users.noreply.github.com>
2023-03-30 14:09:05 +0200

    Gives the extremes of the intersection line

--- SecondaryFiberMesh.m
+++ SecondaryFiberMesh.m
@@ -1,4 +1,4 @@
-function [] = SecondaryFiberMesh(E_eff)
+function [C_edges] = SecondaryFiberMesh(E_eff, V_f_lim)
 
 %% Parameters for Meshgrid
 
@@ -6,13 +6,13 @@ function [] = SecondaryFiberMesh(E_eff)
 nu_m = 0.14;          % [-], Poisson's ratio of the matrix  %functie
 E_fiber =  5.35e+08;  % [Pa], Young's modulus for fiber (PCL) 
 E_matrix = 1.18e+06;  % [Pa], Young's modulus for matrix (silicon
-E_base = 2.89e+06;    % [Pa], Young's modulus of the sample with 9 PCL fibers at 0 degree
+E_base = 2.89e+06;    % [Pa], Young's modulus of the sample with 9 PCL fibers and silicon matrix at 0-degree angle
 
 % Parameters (Orientations)
 theta = linspace(0, 90, 100);  % [degrees], Matrix with ange of angles between the primary (at 0 degrees)
 
 % Parameter (Volume Fractions) 
-V_fiber2 = linspace(0, 1, 100); % [-], Matrix with range of volume fraction of the secondary fibers
+V_fiber2 = linspace(0, V_f_lim, 100); % [-], Matrix with range of volume fraction of the secondary fibers
 
 %% Parameters for reference surfaces in Meshgrid
 % Parameters (Young's modulus of the real pleura) 
@@ -49,20 +49,30 @@ E_diff = E_eff_total - E_effective;
 C = contours(V_f2_mesh, theta_mesh, E_diff, [0 0]);
 
 % Extract the x- and y-locations from the contour matrix C.
-V_f_L = C(1, 2:end);
-theta_L = C(2, 2:end);
+V_f_L = C(1, 2:end); % All the V_f intersection coordinates
+theta_L = C(2, 2:end); % All the theta intersection coordinates
 
 % Interpolate on the first surface to find z-locations for the intersection line.
-ELoc = interp2(V_f2_mesh, theta_mesh, E_eff_total, V_f_L, theta_L);
+ELoc = interp2(V_f2_mesh, theta_mesh, E_eff_total, V_f_L, theta_L)
 
 line(V_f_L, theta_L, ELoc, 'Color', 'r', 'LineWidth', 3);
 
 %% Get the equation for the line
-p = polyfit(V_f_L, theta_L, 1); % Fit a first-degree polynomial (linear model) to the data
+p = polyfit(V_f_L, theta_L, 2); % Fit a second-order polynomial 
 
 % Get slope and intercept
+slope_2 = p(1);
 slope = p(2);
-intercept = p(1);
+intercept = p(3);
 
 % Print equation of the line
-fprintf('Correlation: theta = %.2f*V_f + %.2f\n', slope, intercept);
+fprintf('Correlation: theta = %.2f*V_f^2 + %.2f*V_f + %.2f\n', slope_2, slope, intercept);
+
+%% Define the ranges
+% Extract the extremes in a table
+C_edges = [C(:,2), C(:,(length(C)))];
+V_f_min = C_edges(1,1);
+fprintf('Volume fraction range = %.4f : %.4f', V_f_min, V_f_lim);
+fprintf('\n');
+theta_max = C_edges(2,2);
+fprintf('Fiber angle range = 0 : %.4f', theta_max);
